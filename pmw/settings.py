a44e503ad6c9c3e219740948c9a65d457f5ec02a
frangomez <119343543+erfjgm@users.noreply.github.com>
2024-02-19 18:10:05 +0100

    corregimos un error en models, agregamos la pagina de inicio, menu, pié y agregamos las opciones de login y logout

--- pmw/settings.py
+++ pmw/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Variables de redireccion Login
+LOGIN_REDIRECT_URL = 'home'
--- project_manager/models.py
+++ project_manager/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+    nif = models.CharField(max_length=12)
+    mail = models.EmailField(null=True)
+    address = models.CharField(max_length=255)
+    postalCode = models.CharField(max_length=6)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,6 +14,7 @@ class UserProfile(models.Model):
 
 class Board(models.Model):
     name = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     
 class List(models.Model):
     name = models.CharField(max_length=255)
@@ -18,7 +23,3 @@ class List(models.Model):
 class Card(models.Model):
     name = models.CharField(max_length=255)
     lista = models.ForeignKey(List, on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-
-
-
