dad5f8af574de812de57812f593c5d0a53b67537
frangomez <119343543+erfjgm@users.noreply.github.com>
2024-02-21 20:03:37 +0100

    agregamos la visualizacion de board y intercambio entre boards

--- project_manager/views.py
+++ project_manager/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
+from .models import Board, List, Card, Company, UserProfile
 
 
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        boards = list(Board.objects.filter(company=list(
+            UserProfile.objects.filter(user=request.user))[0].company))
+        if request.method == 'POST':
+            board = request.POST.get('board')
+            print(request.POST.get('board'))
+            if board:
+                board = get_object_or_404(Board, name=board)
+                lists = list(List.objects.filter(board=board))
+                return render(request, 'home.html', {
+                    'title': 'Inicio',
+                    'board': board,
+                    'boards': boards,
+                    'lists': lists,
+                })        
+        lists = list(List.objects.filter(board=boards[0]))
+        return render(request, 'home.html', {
+            'title': 'Inicio',
+            'board': boards[0],
+            'boards': boards,
+            'lists': lists,
+        })
+    else:
+        return render(request, 'home.html', {
+            'title': 'Inicio',
+        })
+
 
 def exit(request):
     logout(request)
